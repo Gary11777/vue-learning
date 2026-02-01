c2aa790528b212124ec332680e5266ded88fa120
Gary11777 <iharushakou@gmail.com>
2026-02-01 11:43:32 +0300

    E16 Named Slots

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -2,8 +2,8 @@ export default {
     template: `
         <form @submit.prevent="add">
             <div class="flex gap-2 mt-5">    
-                <input v-model="newAssignment" placeholder="New Assignment" class="text-black border border-gray-300 rounded-md px-3 py-2 mr-2 focus:outline-none focus:border-blue-500 w-40" />
-                <button type="submit" class="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Add</button>
+                <input v-model="newAssignment" placeholder="New Assignment" class="text-black border border-gray-300 rounded-md px-3 py-2 mr-2 focus:outline-none focus:border-blue-500 w-36" />
+                <button type="submit" class="bg-blue-500 text-white rounded-md px-2 py-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Add</button>
             </div>
         </form>
     `,
@@ -16,7 +16,7 @@ export default {
     methods: {
         add() {
             this.$emit('add', this.newAssignment);
-            
+
             this.newAssignment = '';
         }
     }
